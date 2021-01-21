85b197c89fc1470f6d5484bc867c12d72b51175b
mari-code-l <maridel.arcagua@gmail.com>
2021-01-21 19:02:20 +0000

    working on emailJS

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,10 @@ $('body').noisy({
     intensity: 0.2,
     size: 200,
     opacity: 0.28,
-    randomColors: false, // true by default
+    randomColors: false, 
     color: '#000000'
 });
   
-	//Google Maps JS
-	//Set Map
 	function initialize() {
 			var myLatlng = new google.maps.LatLng(53.3333,-3.08333);
 			var imagePath = 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
@@ -20,15 +18,12 @@ $('body').noisy({
 			}
 
 		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-		//Callout Content
 		var contentString = 'Some address here..';
-		//Set window width + content
 		var infowindow = new google.maps.InfoWindow({
 			content: contentString,
 			maxWidth: 500
 		});
 
-		//Add Marker
 		var marker = new google.maps.Marker({
 			position: myLatlng,
 			map: map,
@@ -40,7 +35,6 @@ $('body').noisy({
 			infowindow.open(map,marker);
 		});
 
-		//Resize Function
 		google.maps.event.addDomListener(window, "resize", function() {
 			var center = map.getCenter();
 			google.maps.event.trigger(map, "resize");
