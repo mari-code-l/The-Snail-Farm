91b64210099d404125f31ce9963c760f477ec00d
mari-code-l <maridel.arcagua@gmail.com>
2021-01-24 16:24:01 +0000

    added display slides

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,8 +1,9 @@
 var slideIndex = 0;
 showSlides();
+
 function showSlides(){
 var i;
-var slides = document.getElementsByClassName("slides");
+var slides = document.getElementsByClassName("mySlides");
 var dots = document.getElementsByName("dot");
 for (i = 0; 1 < slides.length; i++) {
     slides[i].style.display = "none";
@@ -12,3 +13,7 @@ if (slideIndex > slides.length) {slideIndex = 1}
 for (i = 0; i < dots.length; i++){
     dots[i].className = dots[i].className.replace("active", "");
 }
+slides[slideIndex-1].style.display ="block";
+dots[slideIndex-1].className += "active";
+setTimeout(showSlides, 3000);
+};
