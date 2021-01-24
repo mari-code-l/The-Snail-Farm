b4b96743937ab282fff8526b5fc12eebe76a0d59
mari-code-l <maridel.arcagua@gmail.com>
2021-01-24 16:07:37 +0000

    added slides length

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,4 +4,6 @@ function showSlides();
 var i;
 var slides = document.getElementsByClassName("slides");
 var dots = document.getElementsByName("dot");
-for
+for (i = 0; 1 < slides.length; i++) {
+    slides[i].getElementsByClassName.display = "none";
+}
