0c3730e90b7ddd70942e57a1c71835caea954985
mari-code-l <maridel.arcagua@gmail.com>
2021-01-24 16:32:00 +0000

    remove dot animation corrected code

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,19 +1,14 @@
 var slideIndex = 0;
 showSlides();
 
-function showSlides(){
-var i;
-var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByName("dot");
-for (i = 0; 1 < slides.length; i++) {
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000);
 }
-slideIndex++;
-if (slideIndex > slides.length) {slideIndex = 1}
-for (i = 0; i < dots.length; i++){
-    dots[i].className = dots[i].className.replace("active", "");
-}
-slides[slideIndex-1].style.display ="block";
-dots[slideIndex-1].className += "active";
-setTimeout(showSlides, 3000);
-};
