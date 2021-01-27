ba1712d5c65253d32473951092bfc2df5d9a7f36
mari-code-l <maridel.arcagua@gmail.com>
2021-01-27 16:27:10 +0000

    deleted and added elements clean code

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,14 @@
 function sendMail(contactForm) {
     emailjs.send("service_maridel", "snailfarm", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_email": contactForm.email.value,
         "project_request": contactForm.projectsummary.value
     })
     .then(
         function(response) {
             alert("Submitted!");
             contactForm.name.value = ''
-            contactForm.emailaddress.value = ''
+            contactForm.email.value = ''
             contactForm.projectsummary.value = ''
         },
         function(error) {
